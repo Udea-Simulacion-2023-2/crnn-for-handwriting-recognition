397346b50fa333fd17cb1d271be5435ad6edd7ce
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2023-11-16 20:45:39 -0500

    version lfrz api

--- api/main.py
+++ api/main.py
@@ -1,10 +1,50 @@
-from fastapi import FastAPI, UploadFile
+import os
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+from keras import backend as K
+import cv2
+
+from api.utils.Utils import Utils
 
 app = FastAPI()
+utils = Utils()
+
+# Ruta donde se guardarán las imágenes
+image_save_path = "uploaded_images/"
+
+# Verificar si la carpeta existe, si no, crearla
+os.makedirs(image_save_path, exist_ok=True)
+
+# Cargar el modelo CRNN preentrenado
+model = tf.keras.models.load_model("api/model/model_20mil_80epoch.h5", compile=False)
+
+# Ruta para procesar la imagen y realizar la predicción
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    # Guardar la imagen en la raíz del proyecto
+    image_path = os.path.join(image_save_path, file.filename)
+    with open(image_path, "wb") as image_file:
+        image_file.write(file.file.read())
+
+    # Leer la imagen
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    # Preprocesar la imagen
+    preprocessed_image = utils.preprocess(image)
+    img_array = preprocessed_image/255. # Normalizacion [0,1]
+
+    # Realizar la predicción con el modelo CRNN
+    prediction = model.predict(img_array.reshape(1, 256, 64, 1))
+
+    # Decodificar la predicción (ajústalo según el formato de salida de tu modelo)
+    decoded_text = K.get_value(K.ctc_decode(prediction, input_length=np.ones(prediction.shape[0])*prediction.shape[1],greedy=True)[0][0])
+    response = utils.num_to_label(decoded_text[0])
+
+    # Devolver la predicción al frontend
+    return JSONResponse(content={"prediction ": response})
 
 
-@app.post("/api/v1/handwritings/")
-async def recognize_handwriting(image: UploadFile):
-    # TODO: use model to get the actual writing text from the handwriting
-    # (image).
-    return {"writing": image.filename}
+
---
+++ api/utils/Utils.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+
+class Utils:
+    alphabets = u"ABCDEFGHIJKLMNOPQRSTUVWXYZ-' "
+    max_str_len = 24  # max length of input labels
+    num_of_characters = len(alphabets) + 1  # +1 for ctc pseudo blank
+    num_of_timestamps = 64  # max length of predicted labels
+
+    def preprocess(self, img):
+        (h, w) = img.shape
+
+        final_img = np.ones([64, 256]) * 255  # Espacios en blanco
+
+        # crop
+        if w > 256:
+            img = img[:, :256]
+
+        if h > 64:
+            img = img[:64, :]
+
+        final_img[:h, :w] = img
+        return cv2.rotate(final_img, cv2.ROTATE_90_CLOCKWISE)
+
+    def label_to_num(self, label):
+        label_num = []
+        for ch in label:
+            label_num.append(self.alphabets.find(ch))
+
+        return np.array(label_num)
+
+    def num_to_label(self, num):
+        ret = ""
+        for ch in num:
+            if ch == -1:  # CTC Blank
+                break
+            else:
+                ret += self.alphabets[ch]
+        return ret
